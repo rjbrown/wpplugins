cd9cac0bb0341c9991bdd1f35a59d3bd560c0710
Do Work <dowork@dos-macbook-pro.home>
2022-10-30 11:41:22 -0400

    creating a plugin for css automation

---
+++ wp-content/plugins/scroll.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package Scroll Features
+ * @version 0.1
+ */
+/*
+Plugin Name: Bobby Brown
+Plugin URI: #
+Description: Adds scroll Animatetions
+Author: Bobby Brown
+Version: 0.1
+*/
+
+// We need some CSS
+function animate_css() {
+	echo "
+    <style>
+	.fade-in-section {
+	    opacity: 0;
+	    transform: translateY(20vh);
+	    visibility: hidden;
+	    transition: opacity 1200ms ease-out, transform 600ms ease-out,visibility 1200ms ease-out;
+	    will-change: opacity, transform, visibility;
+	}
+	.fade-in-section.is-visible {
+	  opacity: 1;
+	  transform: none;
+	  visibility: visible;
+	}
+	
+</style>
+	";
+}
+
+add_action( 'admin_head', 'animate_css' );
